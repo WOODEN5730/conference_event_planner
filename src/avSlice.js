4f3fa4a972a01638a8b915cf61d00a9ab87a1d66
wooden5730 <wooden5730@msn.com>
2026-09-20 16:38:51 -0400

    Updated avSlice.js

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,53 +1,64 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: [
+    {
+      id: 1,
+      img: "https://pixabay.com/images/download/business-20031_640.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      id: 2,
+      img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      id: 3,
+      img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      id: 4,
+      img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+    {
+      id: 5,
+      img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
+  ],
+};
+
 export const avSlice = createSlice({
   name: "av",
-  initialState: [
-    	{
-		img: "https://pixabay.com/images/download/business-20031_640.jpg",
-		name: "Projectors",
-		cost: 200,
-		quantity: 0,
-	},
-    {
-		img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
-		name: "Speaker",
-		cost: 35,
-		quantity: 0,
-    },
-    {
-		img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
-		name: "Microphones",
-		cost: 45,
-		quantity: 0,
-    },
-    {
-		img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
-		name: "Whiteboards",
-		cost: 80,
-		quantity: 0,
-    },
-
-    {
-		img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
-		name: "Signage",
-		cost: 80,
-		quantity: 0,
-    },
-
-  ],
-
-
+  initialState,
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const item = state.items.find(i => i.id === action.payload);
+      if (item) item.quantity++;
     },
+
     decrementAvQuantity: (state, action) => {
-     
+      const item = state.items.find(i => i.id === action.payload);
+      if (item && item.quantity > 0) item.quantity--;
     },
   },
 });
 
-export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
+export const {
+  incrementAvQuantity,
+  decrementAvQuantity,
+} = avSlice.actions;
 
 export default avSlice.reducer;
